a40bbd4cebdf0e0e3a575ecff2bdbe3207084dfb
Jim Waldo <jiniwaldo@gmail.com>
2015-04-04 16:51:02 -0400

    Functioning version of buildCourseDict.py, which has been expanded (and therefore should probably be renamed) to delete records (rows) to insure that a student can't be identified by the set of courses that he or she has taken. The current version deletes courses prefering to get rid of those with the least activity, although the routine that will randomly pick a course is also available.

--- buildCourseDict.py
+++ buildCourseDict.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+
 __author__ = 'waldo'
 
 from de_id_functions import *
 import sys
+import random
 
 def buildCDict(ulist):
     """
@@ -23,7 +26,7 @@ def buildCDict(ulist):
             clist += ('#' + cid)
             unique = False
         elif not unique:
-            clist = makecanonical(clist)
+            clist = ordercoursestring(clist)
             if clist in cdict:
                 cdict[clist].append(oldid)
             else:
@@ -39,13 +42,24 @@ def buildCDict(ulist):
     print len(cdict.keys())
     return cdict
 
+def ordercoursestring(coursestring):
+    '''
+    Take a string of terms separated by the character '#' and return a string of those terms in
+    sorted order separated by '#'
+    :param coursestring: a string of terms (course names) separated by '#'
+    :return: a string of terms (course names) in order, separated by '#'
+    '''
+    clist = coursestringtolist(coursestring)
+    clist.sort()
+    return courselisttostring(clist)
 
-
-def parseCourseString(coursestring):
+def coursestringtolist(coursestring):
     """
+    Create a list of courses from a string that contains the course names separated by the '#' character
+
     :param coursestring: A string that is the concatenation of names of courses, separated by
     the character '#'
-    :return: A list of the course names, with the seperator character removed
+    :return: A list of the course names, with the separator character removed
     """
     parts = coursestring.partition('#')
     retlist = [parts[0]]
@@ -54,20 +68,75 @@ def parseCourseString(coursestring):
         retlist.append(parts[0])
     return retlist
 
-def makecanonical(coursestring):
-    clist = parseCourseString(coursestring)
-    clist.sort()
-    retstring = clist[0]
+def courselisttostring(courseslist):
+    '''
+    Create a string of course names separated by the '#' character from a list of the course names
+    :param courseslist:
+    :return: a string of the course names in the list, separated by the character '#'
+    '''
+
+    retstring = courseslist[0]
     i = 1
-    while i < len(clist):
-        retstring += ('#' + clist[i])
+    while i < len(courseslist):
+        retstring += ('#' + courseslist[i])
         i += 1
     return retstring
 
+def randomdropclass(student, classlist, c):
+    clist = coursestringtolist(classlist)
+    i = random.randint(0, len(clist))
+    return clist[i]
 
 
-def dropClass(classlist, studentlist, classdict):
-    pass
+def participationdropclass(student, classlist, c):
+    print 'in participationdropclass for student =', student
+    cmd = 'Select * from source where user_id = "' + student + '"'
+    c.execute(cmd)
+    studentrecords = c.fetchall()
+    todrop = studentrecords[0]
+    i = 1
+    while i < len(studentrecords):
+        comprec = studentrecords[i]
+        if todrop[4] > comprec[4]:
+            todrop = comprec
+        elif todrop[5] > comprec[5]:
+            todrop = comprec
+        elif todrop[6] > comprec[6]:
+            todrop = comprec
+        i += 1
+
+    print 'Decided to drop', todrop[0]
+    return todrop[0]
+
+def dropfromlist(todrop, oldlist):
+    clist = coursestringtolist(oldlist)
+    clist.remove(todrop)
+    return courselisttostring(clist)
+
+
+
+def dropClass(classlist, studentlist, classdict, c):
+    print 'entering dropClass with student list', studentlist
+    if len(classdict[classlist]) > 4:
+        return
+    finddropclass = participationdropclass
+    for student in studentlist:
+        dropclass = finddropclass(student, classlist, c)
+        print 'dropping course', dropclass, 'for student', student
+        cmd = 'Delete from source where user_id = ' + '"' + student +'"' + 'AND course_id = ' + '"' + dropclass + '"'
+        c.execute(cmd)
+        print len(classdict[classlist])
+        if len(classdict[classlist]) > 1:
+            classdict[classlist].remove(student)
+        newlist = dropfromlist(dropclass, classlist)
+        if '#' in newlist:
+            if newlist in classdict:
+                classdict[newlist].append(student)
+            if len(classdict[newlist]) < 5:
+                dropClass(newlist, {student}, classdict, c)
+
+    return
+
 
 if __name__ == '__main__':
     dbName = sys.argv[1]
@@ -81,6 +150,6 @@ if __name__ == '__main__':
     for classlist in cdict:
         if len(cdict[classlist]) < 5:
             count += 1
-            #print classlist, len(cdict[classlist]), cdict[classlist]
-            #dropClass(classlist, cdict[classlist], cdict)
+            dropClass(classlist, cdict[classlist], cdict, c)
     print count
+    dbClose(c)
